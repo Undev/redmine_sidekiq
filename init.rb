0d558ba703fc5891a492434ee2f9346a19ffff7e
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-01-21 16:00:22 +0400

    added init.rb

---
+++ init.rb
@@ -0,0 +1,11 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_sidekiq do
+  name        'Sidekiq for Redmine'
+  description 'Background jobs for Redmine'
+  author      'Undev'
+  version     '0.0.1'
+  url         'https://github.com/Undev/redmine_sidekiq'
+
+  requires_redmine :version_or_higher => '2.1'
+end
