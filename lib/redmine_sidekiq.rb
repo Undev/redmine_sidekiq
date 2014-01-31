81bb2eb8a0e8ea1b3cd067756b028a0405a15f9f
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-01-31 13:42:06 +0400

    IssueRelation has a "delay" attribute and to prevent ActiveRecord::DangerousAttributeError we remove sidekiq "delay" method

--- lib/redmine_sidekiq.rb
+++ lib/redmine_sidekiq.rb
@@ -1,5 +1,10 @@
 module RedmineSidekiq
 end
 
+# IssueRelation has a "delay" attribute and
+# to prevent ActiveRecord::DangerousAttributeError
+# we remove sidekiq method:
+ActiveRecord::Base.send :undef_method, :delay
+
 require 'redmine_sidekiq/admin_constraint'
 require 'workers/base_worker'
